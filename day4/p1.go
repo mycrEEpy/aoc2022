c8529efec513845cb4dd77c260508e10b656b12d
Tobias <bvrcreepyx@hotmail.de>
2022-12-04 23:31:37 +0000

    add day4

---
+++ day4/p1.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("day4/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var sum int
+
+	for scanner.Scan() {
+		pairs := strings.Split(scanner.Text(), ",")
+
+		contained, err := isContained(pairs[0], pairs[1])
+		if err != nil {
+			panic(err)
+		}
+
+		if contained {
+			sum++
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func isContained(a, b string) (bool, error) {
+	aMin, aMax, err := splitAndConvertToInt(a)
+	if err != nil {
+		return false, err
+	}
+
+	bMin, bMax, err := splitAndConvertToInt(b)
+	if err != nil {
+		return false, err
+	}
+
+	// full overlap
+	if (aMin >= bMin && bMax <= aMax) ||
+		(aMin >= bMin && aMax <= bMax) {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func splitAndConvertToInt(s string) (int, int, error) {
+	split := strings.Split(s, "-")
+
+	a, err := strconv.Atoi(split[0])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	b, err := strconv.Atoi(split[1])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return a, b, nil
+}
